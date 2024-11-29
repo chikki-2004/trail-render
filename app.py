0fc9cd4b14f00397db0bf1aac05dd071f859e9dc
chikki-2004 <c.hikki25092004@gmail.com>
2024-11-29 20:50:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit, join_room, leave_room
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
 @app.route('/')
 def index():
@@ -12,16 +12,16 @@ def index():
 @socketio.on('join-room')
 def handle_join(data):
     room = data['room']
-    join_room(room)
-    emit('user-connected', {'userId': data['userId']}, to=room)
+    socketio.join_room(room)
+    socketio.emit('user-connected', {'userId': data['userId']}, to=room)
 
 @socketio.on('signal')
 def handle_signal(data):
-    emit('signal', data, to=data['room'])
+    socketio.emit('signal', data, to=data['room'])
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    emit('user-disconnected', broadcast=True)
+    socketio.emit('user-disconnected', broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000)
